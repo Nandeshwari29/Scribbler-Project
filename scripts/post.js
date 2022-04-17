b310d7a177fc8ee668bd0ceadc5263150dab3cc0
Nandeshwari <ranawat.nandeshwari@gmail.com>
2022-04-17 12:22:00 +0530

    commit for POST html file

---
+++ scripts/post.js
@@ -0,0 +1,42 @@
+var editMode=false;
+function addComments(userComments){
+    var addEventName = userComments.value +'<br>';
+    var a = document.getElementById('userComments');
+    document.getElementById('commentsborder').innerHTML += '<div class="comment">'+addEventName+'</div>';          
+    a.value=a.defaultValue;
+}
+//Enter your code here
+var like = 0;
+function postLiked(){
+    document.getElementById("likeBlog").innerHTML='<i class="fa fa-thumbs-up" aria-hidden="true"></i> Liked!';
+    like = like+1;
+    if(like == 1){
+        document.getElementById("commentCount").innerHTML=1 + " person likes this!";
+    }
+    else{
+        document.getElementById("commentCount").innerHTML=like + " people have liked this!";
+    }
+    
+    
+}
+var num=0;
+function edit(){
+    if(!editMode){
+        var savebutton= 'Save<i class="fa fa-save"></i>';
+        document.getElementById('text-h3').style.borderColor='pink';
+        document.getElementById('text-h3').style.borderStyle='solid';
+
+        document.getElementById('paraText').style.borderColor='pink';
+        document.getElementById('paraText').style.borderStyle='solid';
+        document.getElementById('edit-button').innerHTML= savebutton;
+        editMode=true;
+    }
+    else{
+        var value = document.getElementById('text-h3');
+        document.getElementById('text-h3').style.border="none";
+        document.getElementById('paraText').style.border="none";
+        var editbutton= 'Edit<i class="fa fa-edit"></i>';
+        document.getElementById('edit-button').innerHTML= editbutton;
+
+    }
+}
